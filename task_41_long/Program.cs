22d539a951b5c5b7db446dd253d1c0e0dcba4e16
ivan <lopatin.ivan28@icloud.com>
2022-07-17 22:58:12 +0300

    add task 41 long version

---
+++ task_41_long/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void fillArray (int[] arr)      // Метод для ввода чисел пользователем и преобразования их в массив.
+{
+   int length = arr.Length;
+   for (int index = 0; index < length; index++)
+      {
+      Console.WriteLine("Введите число под номером " + index);
+      arr[index] = Convert.ToInt32(Console.ReadLine());
+      }
+}
+
+int countPositive(int[] arr1)   // Метод подсчёта положительных чисел.
+{
+   int count = arr1.Count(i => i > 0);
+   return count;
+}
+
+Console.WriteLine("Введите количество чисел, среди которых вы хотите узнать количество положительных(номерация нацинается с нуля):");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[x];  
+
+fillArray(array);
+
+Console.WriteLine("Введённые вами числа:");
+Console.Write(string.Join(" ", array));
+Console.WriteLine();
+Console.WriteLine("Количество чисел больше нуля: " + countPositive(array));
