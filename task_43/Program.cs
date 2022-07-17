29c9700dcb87be42b0416686d780d60ec927bad8
ivan <lopatin.ivan28@icloud.com>
2022-07-17 22:59:18 +0300

    add task 43

---
+++ task_43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+string findDot (double b1, double k1, double b2, double k2)
+{
+   double x = (b2 - b1) / (k1 - k2);
+   double y = k2 * x + b2;
+   return $"Координаты точки пересечения двух заданных прямых: ({x};{y})";
+}
+
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(findDot(b1, k1, b2, k2));
