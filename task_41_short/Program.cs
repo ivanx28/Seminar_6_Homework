610875421288566a3182d1e39356eb65a61cf593
ivan <lopatin.ivan28@icloud.com>
2022-07-17 22:58:49 +0300

    add task 41 short version

---
+++ task_41_short/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int countPositive(int[] arr1)   // Метод подсчёта положительных чисел.
+{
+   int count = arr1.Count(i => i > 0);
+   return count;
+}
+
+Console.WriteLine("Введите необходимые вам числа через пробел, по окончанию ввода нажмите Enter: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+Console.WriteLine("Количество чисел больше нуля: " + countPositive(array));
